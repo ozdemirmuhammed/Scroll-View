90ecca0308631395becbd07d97638ab51800be97
ozdemirmuhammed <MacBook@Muhammed-MacBook-Pro.local>
2014-05-23 11:55:03 +0300

    Initial Commit

---
+++ Scroll View/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Scroll View
+//
+//  Created by Muhammed on 23.05.2014.
+//  Copyright (c) 2014 Muhammed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
